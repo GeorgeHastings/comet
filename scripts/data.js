e6008e48e136178e35194a80eea0d294c1f6a9ac
georgehastings <g.hastings3@gmail.com>
2016-09-29 15:34:58 -0400

    added

---
+++ scripts/data.js
@@ -0,0 +1,151 @@
+var Data = {
+  colleagues: [
+    {
+      id: 0,
+      name: 'Me',
+      picture: 'alex',
+      commitments: [
+        {
+          who: 1,
+          what: 'Compile Annual Report',
+          why: 'Because',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 2,
+          what: 'Keep lookin good',
+          why: 'Because you deserve it',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 3,
+          what: 'Read that book',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 4,
+          what: 'Watch Narcos',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 5,
+          what: 'Watch Breaking Bad',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+    {
+      id: 1,
+      name: 'Eduardo A',
+      picture: 'man1',
+      commitments: [
+        {
+          who: 0,
+          what: 'Watch Better Call Saul',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 0,
+          what: 'Watch Narcos',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+    {
+      id: 2,
+      name: 'Maria B',
+      picture: 'maria',
+      commitments: [
+        {
+          who: 0,
+          what: 'Watch Outlanders',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 0,
+          what: 'Watch Stranger Things',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+    {
+      id: 3,
+      name: 'Mark G',
+      picture: 'mark',
+      commitments: [
+        {
+          who: 0,
+          what: 'Watch Will and Grace',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 0,
+          what: 'Watch Game of Thrones',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+    {
+      id: 4,
+      name: 'Ricardo G',
+      picture: 'ricardo',
+      commitments: [
+        {
+          who: 0,
+          what: 'Watch Mr. Robot',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 0,
+          what: 'Watch Billions',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: 'Alex R',
+      picture: 'alex',
+      commitments: [
+        {
+          who: 0,
+          what: 'Watch House of Cards',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+        {
+          who: 0,
+          what: 'Watch Harry Potter',
+          why: 'Because its good',
+          success: 'Looks good',
+          when: 'by Friday'
+        },
+      ]
+    },
+  ]
+};
